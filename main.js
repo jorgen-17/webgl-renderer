7812e1bad6f4d06a5f4cb7d1f124da224365c617
typedefJorge <jnone001@fiu.edu>
2016-10-19 19:04:16 -0400

    setup initial project, also got canvas to resize with the viewport and allways be full screen

---
+++ main.js
@@ -0,0 +1,30 @@
+function init(){
+	canvas = document.getElementById("mycanvas");
+	gl = canvas.getContext("webgl", 
+								{
+									alpha: false,
+									antialias: false,
+                   					depth: false
+								});
+	
+	gl.viewport(0, 0, canvas.width, canvas.height);
+	gl.clearColor(0, 0.5, 0, 0.5);
+	gl.clear(gl.COLOR_BUFFER_BIT);
+
+	resizeCanvas();
+}
+
+function resizeCanvas() {
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+
+	drawStuff(); 
+}
+
+window.addEventListener('resize', resizeCanvas, false);
+
+function drawStuff() {
+	gl.viewport(0, 0, canvas.width, canvas.height);
+	gl.clearColor(0, 0.5, 0, 0.5);
+	gl.clear(gl.COLOR_BUFFER_BIT);
+}
