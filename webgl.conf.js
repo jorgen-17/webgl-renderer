163c409f03c82ec109c9fb435abe841e034f1d78
typedefJorge <jnone001@fiu.edu>
2017-02-25 07:42:02 -0500

    lol had excluded karma config file

---
+++ webgl.conf.js
@@ -0,0 +1,50 @@
+// Karma configuration
+// Generated on Mon Apr 11 2016 10:01:58 GMT-0400 (Eastern Daylight Time)
+
+module.exports = function(config) {
+  config.set({
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ["jasmine", "karma-typescript"],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+        //software under test
+        { pattern: "src/*.ts" },
+
+        // tests
+        { pattern: "spec/vector.spec.ts" },
+
+    ],
+
+
+    preprocessors: {
+        "**/*.ts": ["karma-typescript"]
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ["dots", "karma-typescript"],
+
+
+    // enable / disable colors in the its 
+    colors: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: [
+    	'PhantomJS',
+        // "Chrome", 
+        // "Firefox"
+    ],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false
+  });
+}
