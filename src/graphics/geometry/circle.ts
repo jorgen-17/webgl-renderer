ea1abfc38b14fd7dbbbf97da4bef42b5ca939c10
typedefJorge <jnone001@fiu.edu>
2017-03-19 19:44:13 -0400

    part 2 of circle implementation

--- src/graphics/geometry/circle.ts
+++ src/graphics/geometry/circle.ts
@@ -1,26 +1,25 @@
 import { Shape } from "./shape";
-import { ShapeMode } from "./shapeModes";
 import { Point2d } from "./point2d";
 import { Float32Vector } from "../../utils/vector";
 import { BoundingRectangle } from "./boundingRectangle";
-import { Midpoint, ThirdPoints } from "../../../src/graphics/geometry/midpoint";
+import { Midpoint } from "../../../src/graphics/geometry/midpoint";
 
 export class Circle implements Shape
 {
-    private diameter: number;
-    private radius: number;
-
     public verticies: Float32Vector;
     public vertexSize: number;
     public numberOfVerticies: number;
     public glRenderMode: number;
 
+    private center: Point2d;
+    private radius: number;
+
     constructor(point1: Point2d, point2: Point2d, gl: WebGLRenderingContext)
     {
-        this.numberOfVerticies = 50;
+        this.numberOfVerticies = 360;
         let boundingRect = new BoundingRectangle(point1, point2);
-        this.diameter = boundingRect.topRight.x - boundingRect.topLeft.x;
-        this.radius = this.diameter / 2;
+        this.center = Midpoint.between(boundingRect.topLeft, boundingRect.bottomRight);
+        this.radius = boundingRect.topRight.x - boundingRect.topLeft.x / 2;
         let vertexArray = this.populateVerticies(boundingRect);
         this.verticies = new Float32Vector(vertexArray);
         this.vertexSize = 2;
@@ -29,45 +28,24 @@ export class Circle implements Shape
 
     private populateVerticies(boundingRect: BoundingRectangle): Float32Array
     {
-        let numberOfPoints = this.numberOfVerticies * 2
-        let arr = new Float32Array(numberOfPoints);
-        let firstPoint = Midpoint.between(boundingRect.topLeft, boundingRect.bottomLeft);
-        arr[0] = firstPoint.x;
-        arr[1] = firstPoint.y;
-        let lastPoint = Midpoint.between(boundingRect.topRight, boundingRect.bottomRight);
-        arr[numberOfPoints - 2] = lastPoint.x;
-        arr[numberOfPoints - 1] = lastPoint.y;
+        let arr = new Float32Array(this.numberOfVerticies * 2); // 2x the verticies one for x one for y
 
-        // 2 less than because the first and last verticies are added manually outside of this function
-        let verticiesToIterateOver = this.numberOfVerticies - 2;
-        let xIncrement = this.diameter / this.numberOfVerticies;
-        let x = boundingRect.topLeft.x + xIncrement;
-        let insertionIndex = 2; // already inserted at position 0 and 1
+        const radiansInACircle = 2 * Math.PI;
+        const thetaIncrement = radiansInACircle / this.numberOfVerticies;
+        let theta = 0;
+        let insertionIndex = 0;
 
-        for ( let i = 0; i < verticiesToIterateOver / 2; i++)
+        for ( let i = 0; i < this.numberOfVerticies; i++)
         {
-            let y = this.getYForGivenX(x);
-            let negY = -1 * y;
-
+            let x = (Math.cos(theta) * this.radius) + this.center.x;
             arr[insertionIndex] = x;
             insertionIndex++;
+            let y = (Math.sin(theta) * this.radius) + this.center.y;
             arr[insertionIndex] = y;
             insertionIndex++;
-            arr[insertionIndex] = x;
-            insertionIndex++;
-            arr[insertionIndex] = negY;
-            insertionIndex++;
-
-            x += xIncrement;
+            theta += thetaIncrement;
         }
 
         return arr;
-    }
-
-    // circle formula y^2 = r^2 - x^2
-    // returns y for a given x
-    private getYForGivenX(x: number): number
-    {
-        return Math.sqrt((Math.pow(this.radius,2) - Math.pow(x, 2)));
     }
 }
