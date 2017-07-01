4f4f750d5f17e8c7a3c8506b0ee7c7f794fcf087
typedefJorge <jnone001@fiu.edu>
2017-07-01 11:53:42 -0400

    readding karma.conf.js also types got messed up last commit

--- lib/webgl-renderer.d.ts
+++ lib/webgl-renderer.d.ts
@@ -1,12 +1,12 @@
-declare module 'graphics/shapes/point2d' {
+declare module 'webgl-renderer/src/graphics/shapes/point2d' {
 	export interface Point2d {
 	    x: number;
 	    y: number;
 	}
 
 }
-declare module 'graphics/shapes/boundingRectangle' {
-	import { Point2d } from 'graphics/shapes/point2d';
+declare module 'webgl-renderer/src/graphics/shapes/boundingRectangle' {
+	import { Point2d } from 'webgl-renderer/src/graphics/shapes/point2d';
 	export class BoundingRectangle {
 	    topLeft: Point2d;
 	    topRight: Point2d;
@@ -19,7 +19,7 @@ declare module 'graphics/shapes/boundingRectangle' {
 	}
 
 }
-declare module 'utils/vector' {
+declare module 'webgl-renderer/src/utils/vector' {
 	export class Float32Vector {
 	    arr: Float32Array;
 	    size: number;
@@ -30,7 +30,7 @@ declare module 'utils/vector' {
 	}
 
 }
-declare module 'graphics/rgbColor' {
+declare module 'webgl-renderer/src/graphics/rgbColor' {
 	export class RGBColor {
 	    red: number;
 	    green: number;
@@ -39,9 +39,9 @@ declare module 'graphics/rgbColor' {
 	}
 
 }
-declare module 'graphics/shapes/shape' {
-	import { Float32Vector } from 'utils/vector';
-	import { RGBColor } from 'graphics/rgbColor';
+declare module 'webgl-renderer/src/graphics/shapes/shape' {
+	import { Float32Vector } from 'webgl-renderer/src/utils/vector';
+	import { RGBColor } from 'webgl-renderer/src/graphics/rgbColor';
 	export abstract class Shape {
 	    verticies: Float32Vector;
 	    rgbColor: RGBColor;
@@ -54,16 +54,16 @@ declare module 'graphics/shapes/shape' {
 	}
 
 }
-declare module 'utils/tuple' {
+declare module 'webgl-renderer/src/utils/tuple' {
 	export interface Tuple<T1, T2> {
 	    first: T1;
 	    second: T2;
 	}
 
 }
-declare module 'graphics/shapes/midpoint' {
-	import { Point2d } from 'graphics/shapes/point2d';
-	import { Tuple } from 'utils/tuple';
+declare module 'webgl-renderer/src/graphics/shapes/midpoint' {
+	import { Point2d } from 'webgl-renderer/src/graphics/shapes/point2d';
+	import { Tuple } from 'webgl-renderer/src/utils/tuple';
 	export class Midpoint {
 	    static between(point1: Point2d, point2: Point2d): Point2d;
 	}
@@ -72,18 +72,18 @@ declare module 'graphics/shapes/midpoint' {
 	}
 
 }
-declare module 'graphics/precision' {
+declare module 'webgl-renderer/src/graphics/precision' {
 	export enum Precision {
 	    Low = 0,
 	    High = 1,
 	}
 
 }
-declare module 'graphics/shapes/ellipse' {
-	import { Shape } from 'graphics/shapes/shape';
-	import { Point2d } from 'graphics/shapes/point2d';
-	import { Precision } from 'graphics/precision';
-	import { RGBColor } from 'graphics/rgbColor';
+declare module 'webgl-renderer/src/graphics/shapes/ellipse' {
+	import { Shape } from 'webgl-renderer/src/graphics/shapes/shape';
+	import { Point2d } from 'webgl-renderer/src/graphics/shapes/point2d';
+	import { Precision } from 'webgl-renderer/src/graphics/precision';
+	import { RGBColor } from 'webgl-renderer/src/graphics/rgbColor';
 	export class Ellipse extends Shape {
 	    private center;
 	    private horizontalRadius;
@@ -95,33 +95,33 @@ declare module 'graphics/shapes/ellipse' {
 	}
 
 }
-declare module 'utils/contextWrangler' {
+declare module 'webgl-renderer/src/utils/contextWrangler' {
 	export class ContextWrangler {
 	    static getContext(canvas: HTMLCanvasElement): WebGLRenderingContext;
 	}
 
 }
-declare module 'graphics/renderModeMapper' {
+declare module 'webgl-renderer/src/graphics/renderModeMapper' {
 	export type RenderMode = "points" | "lines" | "lineStrip" | "lineLoop" | "triangles" | "triangleStrip" | "triangleFan";
 	export class RenderModeMapper {
 	    static renderModeToWebGlConstant(mode: RenderMode, gl: WebGLRenderingContext): number;
 	}
 
 }
-declare module 'graphics/shapes/shapeMode' {
+declare module 'webgl-renderer/src/graphics/shapes/shapeMode' {
 	export type ShapeMode = "points" | "lines" | "ellipses" | "triangles" | "rectangles" | "hexagons" | "octogons";
 
 }
-declare module 'graphics/drawingMode' {
+declare module 'webgl-renderer/src/graphics/drawingMode' {
 	export enum DrawingMode {
 	    Shapes = 0,
 	    Verticies = 1,
 	}
 
 }
-declare module 'settings' {
-	import { DrawingMode } from 'graphics/drawingMode';
-	import { RGBColor } from 'graphics/rgbColor';
+declare module 'webgl-renderer/src/settings' {
+	import { DrawingMode } from 'webgl-renderer/src/graphics/drawingMode';
+	import { RGBColor } from 'webgl-renderer/src/graphics/rgbColor';
 	export let Settings: {
 	    floatsPerPoint: number;
 	    floatsPerColor: number;
@@ -137,8 +137,8 @@ declare module 'settings' {
 	};
 
 }
-declare module 'graphics/vertexBuffer' {
-	import { Float32Vector } from 'utils/vector';
+declare module 'webgl-renderer/src/graphics/vertexBuffer' {
+	import { Float32Vector } from 'webgl-renderer/src/utils/vector';
 	export class VertexBuffer {
 	    renderMode: number;
 	    verticiesStack: Array<Float32Vector>;
@@ -149,7 +149,7 @@ declare module 'graphics/vertexBuffer' {
 	}
 
 }
-declare module 'math/vector3' {
+declare module 'webgl-renderer/src/math/vector3' {
 	export class Vector3 {
 	    elements: Float32Array;
 	    constructor(source?: Vector3 | null);
@@ -157,16 +157,16 @@ declare module 'math/vector3' {
 	}
 
 }
-declare module 'math/vector4' {
+declare module 'webgl-renderer/src/math/vector4' {
 	export class Vector4 {
 	    elements: Float32Array;
 	    constructor(source?: Vector4 | null);
 	}
 
 }
-declare module 'math/matrix4' {
-	import { Vector3 } from 'math/vector3';
-	import { Vector4 } from 'math/vector4';
+declare module 'webgl-renderer/src/math/matrix4' {
+	import { Vector3 } from 'webgl-renderer/src/math/vector3';
+	import { Vector4 } from 'webgl-renderer/src/math/vector4';
 	export class Matrix4 {
 	    elements: Float32Array;
 	    constructor(source?: Matrix4 | null);
@@ -198,8 +198,8 @@ declare module 'math/matrix4' {
 	}
 
 }
-declare module 'graphics/shapes/point3d' {
-	import { RGBColor } from 'graphics/rgbColor';
+declare module 'webgl-renderer/src/graphics/shapes/point3d' {
+	import { RGBColor } from 'webgl-renderer/src/graphics/rgbColor';
 	export class Point3d {
 	    x: number;
 	    y: number;
@@ -210,8 +210,8 @@ declare module 'graphics/shapes/point3d' {
 	}
 
 }
-declare module 'graphics/camera' {
-	import { Point3d } from 'graphics/shapes/point3d';
+declare module 'webgl-renderer/src/graphics/camera' {
+	import { Point3d } from 'webgl-renderer/src/graphics/shapes/point3d';
 	export class Camera {
 	    private _viewMatrix;
 	    private _eyePosition;
@@ -227,11 +227,11 @@ declare module 'graphics/camera' {
 	}
 
 }
-declare module 'graphics/drawingSettings' {
-	import { ShapeMode } from 'graphics/shapes/shapeMode';
-	import { RenderMode } from 'graphics/renderModeMapper';
-	import { DrawingMode } from 'graphics/drawingMode';
-	import { RGBColor } from 'graphics/rgbColor';
+declare module 'webgl-renderer/src/graphics/drawingSettings' {
+	import { ShapeMode } from 'webgl-renderer/src/graphics/shapes/shapeMode';
+	import { RenderMode } from 'webgl-renderer/src/graphics/renderModeMapper';
+	import { DrawingMode } from 'webgl-renderer/src/graphics/drawingMode';
+	import { RGBColor } from 'webgl-renderer/src/graphics/rgbColor';
 	export interface DrawingSettings {
 	    _shapeMode?: ShapeMode;
 	    _renderMode?: RenderMode;
@@ -242,7 +242,7 @@ declare module 'graphics/drawingSettings' {
 	}
 
 }
-declare module 'utils/dictionary' {
+declare module 'webgl-renderer/src/utils/dictionary' {
 	export interface StringDictionary<T> {
 	    [key: string]: T;
 	}
@@ -251,13 +251,13 @@ declare module 'utils/dictionary' {
 	}
 
 }
-declare module 'graphics/webglRenderer' {
-	import { Shape } from 'graphics/shapes/shape';
-	import { RenderMode } from 'graphics/renderModeMapper';
-	import { ShapeMode } from 'graphics/shapes/shapeMode';
-	import { RGBColor } from 'graphics/rgbColor';
-	import { Camera } from 'graphics/camera';
-	import { DrawingSettings } from 'graphics/drawingSettings';
+declare module 'webgl-renderer/src/graphics/webglRenderer' {
+	import { Shape } from 'webgl-renderer/src/graphics/shapes/shape';
+	import { RenderMode } from 'webgl-renderer/src/graphics/renderModeMapper';
+	import { ShapeMode } from 'webgl-renderer/src/graphics/shapes/shapeMode';
+	import { RGBColor } from 'webgl-renderer/src/graphics/rgbColor';
+	import { Camera } from 'webgl-renderer/src/graphics/camera';
+	import { DrawingSettings } from 'webgl-renderer/src/graphics/drawingSettings';
 	export interface IWebGLRenderer {
 	    color: RGBColor;
 	    backgroundColor: RGBColor;
@@ -318,67 +318,67 @@ declare module 'graphics/webglRenderer' {
 	}
 
 }
-declare module 'graphics/colorMapper' {
-	import { RGBColor } from 'graphics/rgbColor';
+declare module 'webgl-renderer/src/graphics/colorMapper' {
+	import { RGBColor } from 'webgl-renderer/src/graphics/rgbColor';
 	export type Color = "red" | "orange" | "yellow" | "green" | "cyan" | "blue" | "indigo" | "fuchsia" | "white";
 	export class ColorMapper {
 	    static colorToRGBColor(color: Color): RGBColor;
 	}
 
 }
-declare module 'graphics/shapes/line' {
-	import { Shape } from 'graphics/shapes/shape';
-	import { Point2d } from 'graphics/shapes/point2d';
-	import { RGBColor } from 'graphics/rgbColor';
+declare module 'webgl-renderer/src/graphics/shapes/line' {
+	import { Shape } from 'webgl-renderer/src/graphics/shapes/shape';
+	import { Point2d } from 'webgl-renderer/src/graphics/shapes/point2d';
+	import { RGBColor } from 'webgl-renderer/src/graphics/rgbColor';
 	export class Line extends Shape {
 	    constructor(point: Point2d, rgbColor: RGBColor, gl: WebGLRenderingContext);
 	    addVertex(vertex: Point2d): void;
 	}
 
 }
-declare module 'graphics/shapes/triangle' {
-	import { Shape } from 'graphics/shapes/shape';
-	import { Point2d } from 'graphics/shapes/point2d';
-	import { RGBColor } from 'graphics/rgbColor';
+declare module 'webgl-renderer/src/graphics/shapes/triangle' {
+	import { Shape } from 'webgl-renderer/src/graphics/shapes/shape';
+	import { Point2d } from 'webgl-renderer/src/graphics/shapes/point2d';
+	import { RGBColor } from 'webgl-renderer/src/graphics/rgbColor';
 	export class Triangle extends Shape {
 	    constructor(point1: Point2d, point2: Point2d, rgbColor: RGBColor, gl: WebGLRenderingContext);
 	}
 
 }
-declare module 'graphics/shapes/rectangle' {
-	import { Shape } from 'graphics/shapes/shape';
-	import { Point2d } from 'graphics/shapes/point2d';
-	import { RGBColor } from 'graphics/rgbColor';
+declare module 'webgl-renderer/src/graphics/shapes/rectangle' {
+	import { Shape } from 'webgl-renderer/src/graphics/shapes/shape';
+	import { Point2d } from 'webgl-renderer/src/graphics/shapes/point2d';
+	import { RGBColor } from 'webgl-renderer/src/graphics/rgbColor';
 	export class Rectangle extends Shape {
 	    constructor(point1: Point2d, point2: Point2d, rgbColor: RGBColor, gl: WebGLRenderingContext);
 	}
 
 }
-declare module 'graphics/shapes/hexagon' {
-	import { Shape } from 'graphics/shapes/shape';
-	import { Point2d } from 'graphics/shapes/point2d';
-	import { RGBColor } from 'graphics/rgbColor';
+declare module 'webgl-renderer/src/graphics/shapes/hexagon' {
+	import { Shape } from 'webgl-renderer/src/graphics/shapes/shape';
+	import { Point2d } from 'webgl-renderer/src/graphics/shapes/point2d';
+	import { RGBColor } from 'webgl-renderer/src/graphics/rgbColor';
 	export class Hexagon extends Shape {
 	    constructor(point1: Point2d, point2: Point2d, rgbColor: RGBColor, gl: WebGLRenderingContext);
 	    private populateVerticies(boundingRect);
 	}
 
 }
-declare module 'graphics/shapes/octogon' {
-	import { Shape } from 'graphics/shapes/shape';
-	import { Point2d } from 'graphics/shapes/point2d';
-	import { RGBColor } from 'graphics/rgbColor';
+declare module 'webgl-renderer/src/graphics/shapes/octogon' {
+	import { Shape } from 'webgl-renderer/src/graphics/shapes/shape';
+	import { Point2d } from 'webgl-renderer/src/graphics/shapes/point2d';
+	import { RGBColor } from 'webgl-renderer/src/graphics/rgbColor';
 	export class Octogon extends Shape {
 	    constructor(point1: Point2d, point2: Point2d, rgbColor: RGBColor, gl: WebGLRenderingContext);
 	    private populateVerticies(boundingRect);
 	}
 
 }
-declare module 'graphics/shapes/shapeFactory' {
-	import { Shape } from 'graphics/shapes/shape';
-	import { ShapeMode } from 'graphics/shapes/shapeMode';
-	import { Point2d } from 'graphics/shapes/point2d';
-	import { RGBColor } from 'graphics/rgbColor';
+declare module 'webgl-renderer/src/graphics/shapes/shapeFactory' {
+	import { Shape } from 'webgl-renderer/src/graphics/shapes/shape';
+	import { ShapeMode } from 'webgl-renderer/src/graphics/shapes/shapeMode';
+	import { Point2d } from 'webgl-renderer/src/graphics/shapes/point2d';
+	import { RGBColor } from 'webgl-renderer/src/graphics/rgbColor';
 	export class ShapeFactory {
 	    static createShape(point1: Point2d, point2: Point2d, shapeMode: ShapeMode, rgbColor: RGBColor, gl: WebGLRenderingContext): Shape;
 	    private static createEllipse(point1, point2, rgbColor, gl);
@@ -390,24 +390,24 @@ declare module 'graphics/shapes/shapeFactory' {
 
 }
 declare module 'webgl-renderer' {
-	import { IWebGLRenderer, WebGLRenderer } from 'graphics/webglRenderer';
-	import { ContextWrangler } from 'utils/contextWrangler';
-	import { Color, ColorMapper } from 'graphics/colorMapper';
-	import { Point2d } from 'graphics/shapes/point2d';
-	import { Line } from 'graphics/shapes/line';
-	import { Shape } from 'graphics/shapes/shape';
-	import { ShapeFactory } from 'graphics/shapes/shapeFactory';
-	import { ShapeMode } from 'graphics/shapes/shapeMode';
-	import { RenderMode } from 'graphics/renderModeMapper';
-	import { Ellipse } from 'graphics/shapes/ellipse';
-	import { Rectangle } from 'graphics/shapes/rectangle';
-	import { Hexagon } from 'graphics/shapes/hexagon';
-	import { Octogon } from 'graphics/shapes/octogon';
-	import { Triangle } from 'graphics/shapes/triangle';
-	import { RGBColor } from 'graphics/rgbColor';
-	import { Camera } from 'graphics/camera';
-	import { Point3d } from 'graphics/shapes/point3d';
-	import { Settings } from 'settings';
+	import { IWebGLRenderer, WebGLRenderer } from 'webgl-renderer/src/graphics/webglRenderer';
+	import { ContextWrangler } from 'webgl-renderer/src/utils/contextWrangler';
+	import { Color, ColorMapper } from 'webgl-renderer/src/graphics/colorMapper';
+	import { Point2d } from 'webgl-renderer/src/graphics/shapes/point2d';
+	import { Line } from 'webgl-renderer/src/graphics/shapes/line';
+	import { Shape } from 'webgl-renderer/src/graphics/shapes/shape';
+	import { ShapeFactory } from 'webgl-renderer/src/graphics/shapes/shapeFactory';
+	import { ShapeMode } from 'webgl-renderer/src/graphics/shapes/shapeMode';
+	import { RenderMode } from 'webgl-renderer/src/graphics/renderModeMapper';
+	import { Ellipse } from 'webgl-renderer/src/graphics/shapes/ellipse';
+	import { Rectangle } from 'webgl-renderer/src/graphics/shapes/rectangle';
+	import { Hexagon } from 'webgl-renderer/src/graphics/shapes/hexagon';
+	import { Octogon } from 'webgl-renderer/src/graphics/shapes/octogon';
+	import { Triangle } from 'webgl-renderer/src/graphics/shapes/triangle';
+	import { RGBColor } from 'webgl-renderer/src/graphics/rgbColor';
+	import { Camera } from 'webgl-renderer/src/graphics/camera';
+	import { Point3d } from 'webgl-renderer/src/graphics/shapes/point3d';
+	import { Settings } from 'webgl-renderer/src/settings';
 	export { IWebGLRenderer, WebGLRenderer, Settings, ContextWrangler, RGBColor, Color, ColorMapper, ShapeMode, RenderMode, Shape, Ellipse, Triangle, Rectangle, Line, Hexagon, Octogon, Point2d, Point3d, ShapeFactory, Camera };
 
 }
